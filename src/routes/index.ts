bc29327da8694be0944471ca1a794a0b19a3d0a3
Andreas Indra Kurniawan <andreasindrakurniawan@gmail.com>
2024-08-05 01:13:55 +0700

    Feat: add connection and create invitation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -14,10 +14,24 @@ router.get("/", async function (req, res, next) {
 });
 
 router.get("/connections", async function (req, res, next) {
+  try{
     const connectionUrl = `${process.env.ARIES_URL}/connections`;
     const response = await axios.get(connectionUrl);
     const connections = response.data.results;
-    res.render("connections", {connections})
+    const activeConnections = connections.filter((connection: any) => connection.state === "active");
+    const invitationResponse = await axios.post(`${process.env.ARIES_URL}/out-of-band/create-invitation`, {
+      "handshake_protocols": [
+          "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/didexchange/1.0"
+      ],
+      "auto_accept": "auto"
+  });
+  const invitation = JSON.stringify(invitationResponse.data.invitation);
+    res.render("connections", {connections: activeConnections, invitation: invitation});
+  } catch(error:any){
+    console.error(error.message);
+    res.send({ message: "Internal Server Error", error: error }).status(500);
+  }
+    
 })
 
 router.post("/connections/delete", async function (req, res, next) {
@@ -41,6 +55,19 @@ router.get("/identities", async function (req, res, next) {
     const credentials = response.data.results;
     // console.log(credentials)
     res.render("credentials", {credentials, connections});
+})
+
+router.post("/add-connection", async function (req, res, next) {
+  try{
+    const {conn_json} = req.body;
+    const receiveInvitationUrl = `${process.env.ARIES_URL}/out-of-band/receive-invitation`;
+    const response = await axios.post(receiveInvitationUrl, conn_json, {headers: {'Content-Type': 'application/json'}});
+    return res.redirect("/connections");
+  }catch(error:any){
+    console.error(error.message);
+    res.send({ message: "Internal Server Error", error: error }).status(500);
+  }
+  
 })
 
 router.post("/credentials/send", async function (req, res, next) {
